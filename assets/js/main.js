58c3c80a944efea2058c71adddad5efe60d31bd0
christyc14 <56485161+christyc14@users.noreply.github.com>
2021-06-10 21:52:13 +0100

    idk

--- assets/js/main.js
+++ assets/js/main.js
@@ -261,4 +261,20 @@
     })
   });
 
+  // dark theme?
+  // var themeSwitcher = document.getElementById("theme-switcher");
+
+  //   // When our button gets clicked
+  //   themeSwitcher.onclick = function() {
+  //     // Get the current selected theme, on the first run
+  //     // it should be `light`
+  //     var currentTheme = document.documentElement.getAttribute("data-theme");
+
+  //     // Switch between `dark` and `light`
+  //     var switchToTheme = currentTheme === "dark" ? "light" : "dark"
+
+  //     // Set our currenet theme to the new one
+  //     document.documentElement.setAttribute("data-theme", switchToTheme);
+  //   }
+
 })()
